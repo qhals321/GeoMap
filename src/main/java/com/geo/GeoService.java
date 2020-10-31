cfa9b003a317be8f4dc57b2b1554218ff176a347
bomin <bomin_93@naver.com>
2020-10-31 10:06:01 +0900

    ADD: get json data method from naver api

--- src/main/java/com/geo/GeoService.java
+++ src/main/java/com/geo/GeoService.java
@@ -1,6 +1,8 @@
 package com.geo;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -16,32 +18,58 @@ public class GeoService {
     }
 
     public void getLocation(String address){
-        int responseNumber = 0;
+        HttpURLConnection connection;
         try {
             String encodedAddress = URLEncoder.encode(address, "UTF-8");
             String method = "GET";
             String requestUrl = API_URL+encodedAddress;
             URL url = new URL(requestUrl);
-            responseNumber = connect(url, method);
-        } catch (UnsupportedEncodingException | MalformedURLException e) {
+            connection = connect(url, method);
+            String json = getJson(connection);
+
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
 
 
     }
 
 
+    private String getJson(HttpURLConnection connection){
+        int responseNumber = 0;
+        StringBuffer response = new StringBuffer();
+        BufferedReader br;
+        try {
+            responseNumber = connection.getResponseCode();
+            if(responseNumber == 200){
+                br = new BufferedReader(new InputStreamReader(connection.getInputStream(),"UTF-8"));
+            }else{
+                br = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+            }
 
-    private int connect(URL url, String method){
+            String inputLine;
+            while ((inputLine = br.readLine()) != null){
+                response.append(inputLine);
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return response.toString();
+    }
+
+    private HttpURLConnection connect(URL url, String method){
         try {
             HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
             httpURLConnection.setRequestMethod(method);
             httpURLConnection.setRequestProperty("X-NCP-APIGW-API-KEY-ID", accountService.getClientId());
             httpURLConnection.setRequestProperty("X-NCP-APIGW-API-KEY", accountService.getClientSecret());
-            return httpURLConnection.getResponseCode();
+            return httpURLConnection;
         } catch (IOException e) {
             e.printStackTrace();
-            return 0;
+            return null;
         }
     }
 }
